0a93a87ae476c2321aee8617c0aea50aa6d4fe43
GalinShterev <galin.shterev@outlook.com>
2019-12-15 21:54:42 +0200

    Add more elements to the timeline

--- scripts/locations.js
+++ scripts/locations.js
@@ -1,12 +1,26 @@
 var journeyButton = get("#journey-button");
-var headings = get("#headings");
+var headings = get(".headings");
 var journeyTimeline = get("#journey-timeline");
 var journey = get(".journey");
+var imageMain = get(".image-main");
+var test = get(".test");
+var overlay = get(".gradient-overlay")
+var journeyHeadings = get("#journey-headings");
+var background = get("#locations-background");
+var firstImageContainer = get("#first-image-container");
 
 window.onload = function () {
     event(journeyButton, "click", function() {
-        hide([headings, journeyTimeline]);
+        hide([headings, journeyTimeline, overlay]);
         show([journey]);
+        animate(imageMain, "image-main-shrink");
+        
+        event(imageMain, "animationend", function () {
+            show([test]);
+            animate(journeyHeadings, "journey-headings-appear");
+            hide([imageMain]);
+        })
+        
     })
 }
 
@@ -25,4 +39,14 @@ function show(elements) {
     for (let element of elements) {
         element.classList.remove("hidden")
     }
+}
+function addClass(element, targetClass) { 
+    element.classList.add(targetClass);
+}
+function removeClass(element, targetClass) {
+    element.classList.remove(targetClass);
+}
+function animate(element, animationClass) {
+    element.className = "";
+    addClass(element, animationClass);
 }
